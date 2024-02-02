79f8c18cad0a22eeb763b5f9bbd11f3c45dbb834
NuclearFizzler <nuclearfizzler@gmail.com>
2024-02-02 15:37:05 -0800

    Plugin for testing UDP send/receive; receive verified, send needs to be verified via udp2serial

---
+++ aprl-cosmos.gemspec
@@ -0,0 +1,23 @@
+# encoding: ascii-8bit
+
+# Create the overall gemspec
+spec = Gem::Specification.new do |s|
+  s.name = 'APRL-COSMOS'
+  s.summary = 'OpenC3 APRL-COSMOS plugin'
+  s.description = <<-EOF
+    APRL-COSMOS plugin for deployment to OpenC3
+  EOF
+  s.license = 'MIT'
+  s.authors = ['Jason Huang, APRL Electrical']
+  s.email = ['nuclearfizzler@gmail.com']
+  s.homepage = 'https://github.com/OpenC3/openc3'
+  s.platform = Gem::Platform::RUBY
+
+  if ENV['VERSION']
+    s.version = ENV['VERSION'].dup
+  else
+    time = Time.now.strftime("%Y%m%d%H%M%S")
+    s.version = '0.0.0' + ".#{time}"
+  end
+  s.files = Dir.glob("{targets,lib,tools,microservices}/**/*") + %w(Rakefile README.md LICENSE.txt plugin.txt)
+end
---
+++ trafficgenerator.py
@@ -0,0 +1,31 @@
+import socket, struct, time
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 1682
+
+flag = 0
+nottime = 69
+nitroT = 1
+keroInj = 2
+loxInj = 3
+keroMani = 4
+loxMani = 5
+keroFlow = 6
+loxFlow = 7
+enginePress = 8
+loadCell_A = 9
+loadCell_B = 10
+thermocouple_A = 11
+thermocouple_B = 12
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+start = time.time()
+
+for x in range(1, 1000):
+    test = struct.pack("IIIIIIIIIIIIII", flag, nottime, nitroT, keroInj, loxInj, keroMani, loxMani, keroFlow, loxFlow, enginePress, loadCell_A, loadCell_B, thermocouple_A, thermocouple_B)
+        
+    sock.sendto(test, (UDP_IP, UDP_PORT))
+    
+    end = time.time()
+    print(f"{end - start} seconds elapsed, {x} packets sent")
