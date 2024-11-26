c433c491770145ec9d5c003504dc20087bb29f79
NuclearFizzler <nuclearfizzler@gmail.com>
2024-11-26 00:32:42 -0800

    Ok delete the ruby files for real

--- targets/APRL/procedures/procedure.rb
+++
@@ -1,3 +0,0 @@
-# Script Runner test script
-cmd("APRL EXAMPLE")
-wait_check("APRL STATUS BOOL == 'FALSE'", 5)
--- targets/LCELL/procedures/procedure.rb
+++
@@ -1,3 +0,0 @@
-# Script Runner test script
-cmd("LCELL EXAMPLE")
-wait_check("LCELL STATUS BOOL == 'FALSE'", 5)
