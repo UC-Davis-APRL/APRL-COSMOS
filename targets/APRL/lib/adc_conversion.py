9f3c664fd6ef62586fe338eae0c9994a69fb1dcf
NuclearFizzler <nuclearfizzler@gmail.com>
2025-10-27 15:54:53 -0700

    Added batching to APRL target

--- targets/APRL/lib/adc_conversion.py
+++ targets/APRL/lib/adc_conversion.py
@@ -4,8 +4,10 @@ from openc3.conversions.conversion import Conversion
 
 # Custom conversion class
 # See https://docs.openc3.com/docs/configuration/telemetry#read_conversion
+
+
 class AdcConversion(Conversion):
-    def __init__(self, range_psi, offset_psi):
+    def __init__(self, range_kg, scaleFactor_x, offset_kg):
         super().__init__()
         # Should be one of 'INT', 'UINT', 'FLOAT', 'STRING', 'BLOCK'
         self.converted_type = 'FLOAT'
@@ -13,20 +15,40 @@ class AdcConversion(Conversion):
         # Use 0 for 'STRING' or 'BLOCK' where the size can be variable
         self.converted_bit_size = 64
         # Multiplier converting voltages to output values
-        self.range_psi = float(range_psi)
-      # Offset psi
-        self.offset_psi = float(offset_psi)
- 
+        self.range_kg = float(range_kg)
+        self.offset_kg = float(offset_kg)
+        self.scaleFactor_x = float(scaleFactor_x)
+
+    def _convert_one(self, value):
+        # Constants
+        adc_reference_voltage = 5.0  # The reference voltage of ADC
+        pga_gain = 128.0             # The PGA gain
+        excitation_voltage = 5.0     # Load cell excitation voltage
+        sensitivity_mvv = 2.0
+
+        # Calculating full scale output voltage of the load cell in mV
+        full_scale_output_mV = excitation_voltage * sensitivity_mvv * self.scaleFactor_x
+
+        # Perform sign extension from 24-bit to 32-bit if necessary
+        if (value & 0x00800000) != 0:  # Check if the 24th bit is set (negative number)
+            value |= 0xFF000000  # Extend the sign to the 32-bit integer
+        
+        # Convert ADC count to voltage considering the PGA gain
+        # Adjusting the calculation to correctly scale bipolar ADC output from -8388608 to +8388607
+        voltage_measured = (float(value) / 8388608.0) * (adc_reference_voltage / pga_gain)
+
+        # Convert voltage to millivolts
+        mv = voltage_measured * 1000.0
+
+        return (mv / full_scale_output_mV) * self.range_kg + self.offset_kg
 
     # @param value [Object] Value based on the item definition. This could be
     #   a string, integer, float, or array of values.
     # @param packet [Packet] The packet object where the conversion is defined
     # @param buffer [String] The raw packet buffer
     def call(self, value, packet, buffer):
-        
-        # Perform conversion logic directly on value
-        # Used when conversion is applied to a regular (not DERIVED) item
-        # NOTE: You can also use packet.read("ITEM") to get additional values
-        
-        return ((value * (5.0 / 8388608)) - 0.48) / 1.92 * self.range_psi + self.offset_psi
 
+        # value is a list for ARRAY_ITEMs; support both list and scalar
+        if isinstance(value, (list, tuple)):
+            return [self._convert_one(x) for x in value]
+        return self._convert_one(value)
